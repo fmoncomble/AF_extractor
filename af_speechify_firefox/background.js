410b0167202106526e49334393e68e5d1c6de993
fmoncomble <59739627+fmoncomble@users.noreply.github.com>
2023-08-18 18:57:47 +0200

    Delete af_speechify_firefox directory

--- af_speechify_firefox/background.js
+++
@@ -1,9 +0,0 @@
-browser.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === 'getTabUrl') {
-    browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const tab = tabs[0];
-      sendResponse({ url: tab.url });
-    });
-    return true; // Keep the message channel open for sendResponse
-  }
-});
