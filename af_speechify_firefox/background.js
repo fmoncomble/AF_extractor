c5602298533e4ae741bfcce4788285da0c007f67
fmoncomble <59739627+fmoncomble@users.noreply.github.com>
2023-08-18 18:10:30 +0200

    Add files via upload

---
+++ af_speechify_firefox/background.js
@@ -0,0 +1,9 @@
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === 'getTabUrl') {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const tab = tabs[0];
+      sendResponse({ url: tab.url });
+    });
+    return true; // Keep the message channel open for sendResponse
+  }
+});
