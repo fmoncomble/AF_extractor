58fa42ca83dfccb5c34c1f8ab31b69ef99438a39
fmoncomble <59739627+fmoncomble@users.noreply.github.com>
2023-08-19 10:55:18 +0200

    Update af_speechify.js

--- af_speechify_firefox/popup/af_speechify.js
+++ af_speechify_firefox/popup/af_speechify.js
@@ -28,6 +28,18 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+async function generateUniqueZipFileName(baseName, existingFileNames) {
+  let fileName = baseName + '.zip';
+  let index = 1;
+
+  while (existingFileNames.has(fileName)) {
+    fileName = `${baseName}_${index}.zip`;
+    index++;
+  }
+
+  return fileName;
+}
+
 async function performExtractAndSave(url) {
   const parser = new DOMParser();
   const response = await fetch(url);
@@ -96,6 +108,7 @@ async function performExtractAndSave(url) {
   const zipBlob = await zip.generateAsync({ type: 'blob' });
 
   const zipFileName = 'xml_archive.zip';
+  const zipFileName = await generateUniqueZipFileName(zipBaseFileName, addedFileNames);
   const downloadPromise = new Promise((resolve, reject) => {
     browser.downloads.download({
       url: URL.createObjectURL(zipBlob),
