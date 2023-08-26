e516cbf1b316bfa5724586816bdf3a5ae3f36442
fmoncomble <florent.moncomble@gmail.com>
2023-08-26 21:56:31 +0200

    UI improvements

--- af_speechify_chrome/content_script.js
+++ af_speechify_chrome/content_script.js
@@ -7,10 +7,32 @@ extractButton.id = 'extractButton';
 extractButton.textContent = 'Extraire';
 anchor.appendChild(extractButton);
 
+// Create a container for the extraction message and spinner
+const extractionContainer = document.createElement('div');
+extractionContainer.id = 'extractionContainer';
+extractionContainer.style.display = 'none'; // Hide initially
+anchor.appendChild(extractionContainer);
+
+// Create the extraction message element
+const extractionMessage = document.createElement('div');
+extractionMessage.id = 'extractionMessage';
+extractionMessage.textContent = 'Extraction en cours…';
+extractionContainer.appendChild(extractionMessage);
+
+// Create the loading spinner element
+const spinner = document.createElement('div');
+spinner.classList.add('spinner'); // Add a class for styling
+extractionContainer.appendChild(spinner);
 
 // Message passing to notify the background script when the button is clicked
 extractButton.addEventListener('click', () => {
+  // Show the extraction container
+  extractionContainer.style.display = 'block';
+
   chrome.runtime.sendMessage({ action: 'performExtraction', url: window.location.href }, response => {
+    // Hide the extraction container
+    extractionContainer.style.display = 'none';
+
     if (response.success) {
       // Display the downloaded files
       const downloadedFilesContainer = document.createElement('div');
@@ -23,4 +45,3 @@ extractButton.addEventListener('click', () => {
     }
   });
 });
-
